b641e3a5559661269b55fb1b06aba62ab7d93fd7
nahyan007 <shorifrex@gmail.com>
2023-05-22 20:41:14 +0600

    card

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+import Product from "../Product/Product";
 
 const Shop = () => {
-    const [products,setProducts] = useState([]);
-    useEffect(()=>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                <h2>products parts:{products.length}</h2>
-            </div>
-            <div className="cart-container">
-                <h4>order summery</h4>
-            </div>
-            
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product 
+          key={product.id} 
+          product={product}>
+          </Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h4>order summery</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
