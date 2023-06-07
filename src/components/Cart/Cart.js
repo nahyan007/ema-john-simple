a84e7a15e90aab87fb0db52389f6005f33e27873
nahyan007 <shorifrex@gmail.com>
2023-06-07 20:47:57 +0600

    order summery

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = (props) => {
+
+    const {cart} = props;
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for (const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+
+    const tax = totalPrice *7/100; 
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
+
+  return (
+    <div className="cart">
+      <h4>order summery</h4>
+      <p>selected item: {cart.length}</p>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax}</p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+    </div>
+  );
+};
+
+export default Cart;
